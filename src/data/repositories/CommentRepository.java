a11d4bcd6cf09eda423b3b48569f549310332261
QuadKing <Allwellsys@gmail.com>
2023-02-14 11:15:03 +0100

    Initial Commit

---
+++ src/data/repositories/CommentRepository.java
@@ -0,0 +1,24 @@
+//package data.repositories;
+//
+//        import data.models.Article;
+//
+//        import java.util.List;
+//
+//public interface CommentRepository {
+//
+//    Article save(Comment comment);
+//
+//    Article findById(int id);
+//
+//    long count();
+//
+//    List<Article> findAll();
+//
+//    void delete(Comment comment);
+//
+//    void delete(int id);
+//
+//    void deleteAll();
+//
+//
+//}
---
+++ test/data/repositories/ArticleRepositoryImplTest.java
@@ -0,0 +1,61 @@
+package data.repositories;
+
+import data.models.Article;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleRepositoryImplTest {
+    private ArticleRepository articleRepository;
+    private Article article;
+
+    @BeforeEach
+    public void setUp(){
+        articleRepository = new ArticleRepositoryImpl();
+        article = new Article();
+        article.setId(1);
+        article.setTitle("Article Title");
+        article.setBody("Article body, document");
+
+    }
+
+
+    @Test public void saveOneArticle_countIsOneTest(){
+        Article article = new Article();
+        articleRepository.save(article);
+        assertEquals (1, articleRepository.count());
+    }
+
+
+    @Test public void saveOneArticle_idOfUserIsOneTest(){
+        Article savedArticle = articleRepository.save(article);
+        assertEquals(1, savedArticle.getId());
+    }
+
+    @DisplayName("Update article test")
+    @Test public void saveTwoArticlesWithSameId_countIsOneTest(){
+        Article savedArticle = articleRepository.save(article);
+        assertEquals(1, articleRepository.count());
+        savedArticle.setId(1);
+        articleRepository.save(savedArticle);
+        assertEquals(1, articleRepository.count());
+    }
+
+    @Test public void saveOneArticle_findArticleByIdTest(){
+        Article savedArticle = articleRepository.save(article);
+        assertEquals(1, savedArticle.getId());
+        Article foundArticle= articleRepository.findById(1);
+        assertEquals(foundArticle, savedArticle);
+
+    }
+
+    @Test public void saveOneArticle_deleteOneArticle_countIsZero(){
+        articleRepository.save(article);
+        assertEquals(1, articleRepository.count());
+        articleRepository.delete(1);
+        assertEquals(0, articleRepository.count());
+    }
+
+
+}
